a06642e60f94476da40268ce21913b2a628dbb41
Bakyt <abdikalykbakytkeldy@gmail.com>
2026-01-20 13:50:40 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String fullName = "Абдыкалык Бакыткелды";
+        String group = "ИС-21-25";
+        String program = "Программное обеспечение";
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("Специальность: " + program);
+    }
+}
